06a5f5d18688356ac535b8fab70d977d70e3d6d9
manish25 <tamanish25@gmail.com>
2022-08-30 14:19:18 +0200

    First React js code

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Manish has started Learning React.          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to my first react program.</p>
+     
       </header>
     </div>
   );
